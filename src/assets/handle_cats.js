277673d8bca0a2d5da5e797994e4dcaaf63e69e3
Ali Alnasser <47244604+AlroySparkle@users.noreply.github.com>
2026-06-13 13:30:02 +0300

    costumize ability and target

--- src/assets/handle_cats.js
+++ src/assets/handle_cats.js
@@ -9,7 +9,68 @@ export const get_cats_list = async () => {
       new_list[cat_key] = {};
     }
     new_list[cat_key][current_cat.form] = current_cat;
+    new_list[cat_key][current_cat.form].against = get_cat_against(current_cat);
+    new_list[cat_key][current_cat.form].target = get_cat_targets(current_cat);
+    new_list[cat_key][current_cat.form].abilities =
+      get_cat_abilities(current_cat);
     return new_list;
   }, {});
   return serve_list_cats;
+};
+
+const get_cat_abilities = (cat) => {
+  const abilities = [];
+  const raw_abilities = cat.ability.split("u2");
+
+  if (raw_abilities.length > 1) {
+    for (
+      let block_index = 1;
+      block_index < raw_abilities.length;
+      block_index++
+    ) {
+      const splited_abilities = raw_abilities[block_index].split("b1");
+      for (
+        let ability_index = 1;
+        ability_index < splited_abilities.length;
+        ability_index++
+      ) {
+        const abilityName = splited_abilities[ability_index]
+          .split("b2")[0]
+          .trim();
+        const isImmunity =
+          raw_abilities[block_index - 1].includes("Not affected by");
+        abilities.push(abilityName + (isImmunity ? " immunity" : ""));
+      }
+    }
+  }
+
+  return abilities;
+};
+
+const get_cat_targets = (cat) => {
+  const target = [cat.target];
+  if (cat.ability && cat.ability.includes("Multi-hit")) {
+    target.push("Multi-hit");
+  }
+  if (cat.ability && cat.ability.includes("Long Distance")) {
+    target.push("Long Distance");
+  }
+  if (cat.ability && cat.ability.includes("Omni Strike")) {
+    target.push("Omni Strike");
+  }
+  return target;
+};
+
+const get_cat_against = (cat) => {
+  if (cat.ability && cat.ability.includes("Against") != -1) {
+    const index = cat.ability.indexOf("Against");
+    const raw_ability = cat.ability.slice(
+      index + "Against".length,
+      cat.ability.length,
+    );
+    const end_against = raw_ability.indexOf("u2");
+    const block_against = raw_ability.slice(1, end_against).split(" ");
+    return block_against;
+  }
+  return [];
 };
